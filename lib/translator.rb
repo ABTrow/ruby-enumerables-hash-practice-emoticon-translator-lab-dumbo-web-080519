7f6f4d902d66b2e2f336e6a76336f77b45af5723
Austin Trow <austintrow@gmail.com>
2019-07-03 04:27:08 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,7 +19,7 @@ end
 
 def get_japanese_emoticon(file, emoticon)
   library = load_library(file)
-  library["get_emoticon"].fetch(emoticon, "nil")
+  library["get_emoticon"].fetch(emoticon, "Sorry, that emoticon was not found")
 end
 
 def get_english_meaning
